ba566126a04576983d2c0259a6b5f258356c241d
Farid LANNABI <46403541+farheinheigt@users.noreply.github.com>
2024-12-03 15:00:31 +0100

    feat ✨: Ajout de la fonction pour rechercher des exploits et amélioration de l'affichage des résultats

--- ctfoutu.py
+++ ctfoutu.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
-import re
 import requests
 import sys
+import time
+import os
+import json
 from pathlib import Path
 from rich.console import Console
 from rich.table import Table
 from datetime import datetime
-import time
+
 
 # Variables globales
-home = str(Path.home())
 header = {"User-Agent": "Mozilla/5.0", "X-Requested-With": "XMLHttpRequest"}
 console = Console()
 
-def rechercher_cves(arg):
+def rechercher_cves_et_exploits(arg):
     # Recherche des CVEs correspondant à une chaîne de caractères donnée via l'API NVD
     console.print(f"[bold yellow]Recherche des derniers CVEs liés à '{arg}'[/bold yellow]")
     try:
@@ -51,13 +52,43 @@ def rechercher_cves(arg):
 
         # Trier les résultats par date de publication (le plus récent en premier)
         resultats_recherche = sorted(resultats_recherche, key=lambda x: datetime.strptime(x[5], "%Y-%m-%d"), reverse=True)
-        afficher_resultats_recherche(resultats_recherche)
+        afficher_resultats_recherche(resultats_recherche, "CVEs")
+        sauvegarder_resultats(resultats_recherche, "CVEs")
     except Exception as e:
         console.print(f"[bold red]Erreur lors de la recherche des CVEs :[/bold red] {e}")
 
-def afficher_resultats_recherche(resultats):
-    # Affiche les résultats de la recherche de CVEs dans un tableau bien formaté
-    tableau = Table(title="Résultats de la recherche pour les CVEs")
+    # Recherche des exploits correspondant à une chaîne de caractères donnée dans la base de données locale
+    console.print(f"[bold yellow]Recherche des exploits liés à '{arg}'[/bold yellow]")
+    try:
+        fichier_exploits = "./files_exploits.csv"
+        tableau = Table(title=f"Résultats de la recherche pour les exploits : {arg}")
+        tableau.add_column("EDB", style="bold red")
+        tableau.add_column("Langage", style="bold")
+        tableau.add_column("Description", style="yellow")
+        tableau.add_column("Auteur", style="cyan")
+        tableau.add_column("Date de publication", style="green")
+        tableau.add_column("Mise à jour", style="blue")
+
+        correspondances_exploits = list(obtenir_correspondances_exploits(arg, fichier_exploits))  # Convertir le générateur en liste pour éviter les problèmes de fichier fermé
+        # Trier les exploits par date de publication (le plus récent en premier)
+        correspondances_exploits = sorted(correspondances_exploits, key=lambda x: x.split(",")[3], reverse=True)
+        resultats_exploits = []
+        for exploit in correspondances_exploits:
+            details_exploit = exploit.split(",")
+            resultats_exploits.append((details_exploit[0], obtenir_langage_exploit(details_exploit[1]), details_exploit[2][:100].strip('"'), details_exploit[4].strip('"'), details_exploit[3], details_exploit[9]))
+            tableau.add_row(f"EDB-{details_exploit[0]}", obtenir_langage_exploit(details_exploit[1]), details_exploit[2][:100].strip('"'), details_exploit[4].strip('"'), details_exploit[3], details_exploit[9])
+        console.print(tableau)
+        sauvegarder_resultats(resultats_exploits, "Exploits")
+    except Exception as e:
+        console.print(f"[bold red]Erreur lors de la recherche des exploits :[/bold red] {e}")
+    finally:
+        # Supprimer le fichier local après utilisation
+        if os.path.exists(fichier_exploits):
+            os.remove(fichier_exploits)
+
+def afficher_resultats_recherche(resultats, titre):
+    # Affiche les résultats de la recherche dans un tableau bien formaté
+    tableau = Table(title=f"Résultats de la recherche pour les {titre}")
     tableau.add_column("CVE", style="bold red")
     tableau.add_column("CVSS", style="bold")
     tableau.add_column("Fournisseur", style="cyan")
@@ -70,6 +101,29 @@ def afficher_resultats_recherche(resultats):
         score_couleur = obtenir_couleur_cvss(score)
         tableau.add_row(resultat[0], score_couleur, resultat[2].upper()[:20], resultat[3][:20], resultat[4][:100], resultat[5])
     console.print(tableau)
+
+def sauvegarder_resultats(resultats, titre):
+    # Sauvegarder les résultats de la recherche au format Markdown et JSON
+    nom_fichier_md = f"resultats_{titre.lower()}.md"
+    nom_fichier_json = f"resultats_{titre.lower()}.json"
+    
+    try:
+        # Sauvegarder en Markdown
+        with open(nom_fichier_md, "w") as fichier_md:
+            fichier_md.write(f"# Résultats de la recherche pour les {titre}\n\n")
+            fichier_md.write("| CVE/EDB | CVSS/Langage | Fournisseur | Produit | Description | Mise à jour |\n")
+            fichier_md.write("|----------|-------------|------------|---------|-------------|-------------|\n")
+            for resultat in resultats:
+                ligne = f"| {resultat[0]} | {resultat[1]} | {resultat[2]} | {resultat[3]} | {resultat[4]} | {resultat[5]} |\n"
+                fichier_md.write(ligne)
+        console.print(f"[bold green]Résultats sauvegardés en Markdown : {nom_fichier_md}[/bold green]")
+        
+        # Sauvegarder en JSON
+        with open(nom_fichier_json, "w") as fichier_json:
+            json.dump(resultats, fichier_json, indent=4)
+        console.print(f"[bold green]Résultats sauvegardés en JSON : {nom_fichier_json}[/bold green]")
+    except IOError as e:
+        console.print(f"[bold red]Erreur lors de la sauvegarde des résultats :[/bold red] {e}")
 
 def obtenir_couleur_cvss(score):
     # Détermine la couleur à afficher en fonction du score CVSS
@@ -86,32 +140,10 @@ def obtenir_couleur_cvss(score):
     except ValueError:
         return "[white]N/A[/white]"
 
-def rechercher_exploits(arg):
-    # Recherche des exploits correspondant à une chaîne de caractères donnée dans la base de données locale
-    console.print(f"[bold yellow]Recherche des exploits liés à '{arg}'[/bold yellow]")
-    try:
-        tableau = Table(title=f"Résultats de la recherche pour les exploits : {arg}")
-        tableau.add_column("EDB", style="bold red")
-        tableau.add_column("Langage", style="bold")
-        tableau.add_column("Description", style="yellow")
-        tableau.add_column("Auteur", style="cyan")
-        tableau.add_column("Date de publication", style="green")
-        tableau.add_column("Mise à jour", style="blue")
-
-        correspondances_exploits = list(obtenir_correspondances_exploits(arg))  # Convertir le générateur en liste pour éviter les problèmes de fichier fermé
-        # Trier les exploits par date de publication (le plus récent en premier)
-        correspondances_exploits = sorted(correspondances_exploits, key=lambda x: x.split(",")[3], reverse=True)
-        for exploit in correspondances_exploits:
-            details_exploit = exploit.split(",")
-            tableau.add_row(f"EDB-{details_exploit[0]}", obtenir_langage_exploit(details_exploit[1]), details_exploit[2][:100].strip('"'), details_exploit[4].strip('"'), details_exploit[3], details_exploit[9])
-        console.print(tableau)
-    except Exception as e:
-        console.print(f"[bold red]Erreur lors de la recherche des exploits :[/bold red] {e}")
-
-def obtenir_correspondances_exploits(arg):
+def obtenir_correspondances_exploits(arg, fichier_exploits):
     # Récupère les exploits correspondants à partir du fichier CSV local
     try:
-        with open(f"{home}/.local/share/cve-maker/files_exploits.csv", "r") as fichier:
+        with open(fichier_exploits, "r") as fichier:
             return [ligne for ligne in fichier if arg.casefold() in ligne.casefold()]
     except IOError as e:
         console.print(f"[bold red]Erreur de lecture/écriture du fichier :[/bold red] {e}")
@@ -137,9 +169,8 @@ def obtenir_langage_exploit(chemin):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        console.print("[bold red]Usage : cve.py <terme-de-recherche>")
+        console.print("[bold red]Usage : ctfoutu.py <terme-de-recherche>")
         sys.exit(1)
     
     terme_recherche = " ".join(sys.argv[1:])
-    rechercher_cves(terme_recherche)
-    rechercher_exploits(terme_recherche)
+    rechercher_cves_et_exploits(terme_recherche)
